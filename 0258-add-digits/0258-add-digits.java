1665ecb59d0a8173d31f54deb89e4bd91f62374b
atul-chandravanshi <atulchandravanshi4580@gmail.com>
2024-12-12 22:46:38 +0530

    Time: 0 ms (100%), Space: 40.9 MB (52.41%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -2,11 +2,11 @@ class Solution {
     public int addDigits(int num) {
         
         if (num == 0) {
-            return 0; // Edge case for 0
+            return 0; 
         } else if (num % 9 == 0) {
-            return 9; // Special case when divisible by 9
+            return 9; 
         } else {
-            return num % 9; // General case
+            return num % 9;
         }
     
     }
